5f58b27137be2b3fc959fcac702cf05431e5ea38
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2018-12-15 13:55:25 +0100

    Fix React undefined error

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { Component } from 'react'
+import React, { Component } from 'react'
 import { View, StyleSheet } from 'react-native'
 import { Constants, MapView, Location, Permissions } from 'expo'
 
