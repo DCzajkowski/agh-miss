b65663747a5c8274cd3007632f848d3f8e627348
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2019-01-07 23:18:03 +0100

    Working demo

--- App.js
+++ App.js
@@ -1,9 +1,36 @@
 import React, { Component } from 'react'
 import { View, StyleSheet } from 'react-native' // AsyncStorage
 import { Constants, MapView, Location, Permissions } from 'expo'
-// import _ from 'lodash'
+import flatten from 'lodash/flatten'
 
 const BASE_URL = 'https://mobtracer.herokuapp.com'
+
+function fuzzyDate(date) {
+  const delta = Math.round((+new Date - date) / 1000)
+
+  const minute = 60
+  const hour = minute * 60
+  const day = hour * 24
+  const week = day * 7
+
+  if (delta < 30) {
+    return 'Just now.'
+  } else if (delta < minute) {
+    return delta + ' seconds ago.'
+  } else if (delta < 2 * minute) {
+    return '1 minute ago.'
+  } else if (delta < hour) {
+    return Math.floor(delta / minute) + ' minutes ago.'
+  } else if (Math.floor(delta / hour) == 1) {
+    return '1 hour ago.'
+  } else if (delta < day) {
+    return Math.floor(delta / hour) + ' hours ago.'
+  } else if (delta < day * 2) {
+    return 'yesterday'
+  }
+
+  return ''
+}
 
 export default class App extends Component {
   state = {
@@ -36,7 +63,7 @@ export default class App extends Component {
   //   let user
 
   //   try {
-  //     user = await AsyncStorage.getItem('user');
+  //     user = await AsyncStorage.getItem('user')
 
   //     if (user !== null) {
   //       this.setState({ user })
@@ -47,7 +74,7 @@ export default class App extends Component {
   //   user = String(Math.random())
 
   //   try {
-  //     await AsyncStorage.setItem('user', user);
+  //     await AsyncStorage.setItem('user', user)
   //   } catch (error) {}
 
   //   this.setState({ user })
@@ -58,7 +85,7 @@ export default class App extends Component {
       if (!this.state.viewsHistory) {
         this.fetchLocations()
       }
-      // this.postLocationAsync()
+      this.postLocationAsync()
     }, 5000)
   }
 
@@ -83,18 +110,18 @@ export default class App extends Component {
 
   fetchLocations = async () => {
     const response = await fetch(`${BASE_URL}/locations`)
-    const data = await response.json()
+    const markers = await response.json()
 
-    const dates = Object.keys(data).sort().reverse()
+    // const dates = Object.keys(data).sort().reverse()
 
-    const locations = [...data[dates[0]], ...data[dates[1]], ...data[dates[2]]]
-    const markers = []
+    // const locations = [...data[dates[0]], ...data[dates[1]], ...data[dates[2]]]
+    // const markers = []
 
-    for (const location of locations) {
-      if (!markers.find(({ user }) => user == location.user)) {
-        markers.push(location)
-      }
-    }
+    // for (const location of locations) {
+    //   if (!markers.find(({ user }) => user == location.user)) {
+    //     markers.push(location)
+    //   }
+    // }
 
     this.setState({ markers })
   }
@@ -114,23 +141,60 @@ export default class App extends Component {
   }
 
   markerClickHandler = async ({ user }) => {
-    this.setState({ viewsHistory: true })
+    this.setState({ viewsHistory: true, markers: [] })
 
     const response = await fetch(`${BASE_URL}/locations/${user}`)
-    const data = await response.json()
+    const markers = await response.json()
 
-    this.setState({ markers: Object.entries(data).map(([key, value]) => value[0]) })
+    this.setState({ markers })
+  }
+
+  mapClickHandler = () => {
+    if (this.state.viewsHistory) {
+      this.setState({ viewsHistory: false })
+      this.fetchLocations()
+    }
   }
 
   render() {
     let mapContents = null
 
     if (this.state.viewsHistory) {
-      mapContents = (
+      // mapContents = (
+      //   <MapView.Polyline
+      //     coordinates={this.state.markers}
+      //   />
+      // )
+
+      mapContents = this.state.markers.map(list => (
         <MapView.Polyline
-          coordinates={this.state.markers}
+          coordinates={list}
         />
-      )
+      ))
+    }
+
+    let markersContent
+
+    if (! this.state.viewsHistory) {
+      markersContent = this.state.markers.map(marker => (
+        <MapView.Marker
+          key={marker.id}
+          coordinate={marker}
+          title={marker.user}
+          description={fuzzyDate(new Date(marker.created_at))}
+          onPress={this.markerClickHandler.bind(this, marker)}
+        />
+      ))
+    } else {
+      markersContent = flatten(this.state.markers).map(marker => (
+        <MapView.Marker
+          key={marker.id}
+          coordinate={marker}
+          title={marker.user}
+          description={fuzzyDate(new Date(marker.created_at))}
+          onPress={this.markerClickHandler.bind(this, marker)}
+        />
+      ))
     }
 
     return (
@@ -139,17 +203,10 @@ export default class App extends Component {
           style={{ alignSelf: 'stretch', flex: 1 }}
           region={this.mapRegion}
           onRegionChange={this.onRegionChange}
+          onPress={this.mapClickHandler}
         >
           {mapContents}
-          {this.state.markers.map(marker => (
-            <MapView.Marker
-              key={marker.id}
-              coordinate={marker}
-              title="My Marker"
-              description="Some description"
-              onPress={this.markerClickHandler.bind(this, marker)}
-            />
-          ))}
+          {markersContent}
         </MapView>
       </View>
     )
